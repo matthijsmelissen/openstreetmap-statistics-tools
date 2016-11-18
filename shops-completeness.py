eee7407e7d7f34fca1e7efd695d39c8a3789a5f1
Matthijs Melissen <info@matthijsmelissen.nl>
2016-11-18 21:48:14 +0100

    Explicitly import sys, clean whitespace

--- shops-completeness.py
+++ shops-completeness.py
@@ -8,7 +8,7 @@
 #
 # Before running this script, you will need to load OSM-data into an
 # PostGIS-database with osm2pgsql:
-# sudo -u postgres createdb --encoding=UTF8 gis    
+# sudo -u postgres createdb --encoding=UTF8 gis
 # psql --dbname=gis -c "CREATE EXTENSION postgis;"
 # psql --dbname=gis -c "CREATE EXTENSION hstore;"
 # osm2pgsql --slim -d gis -C 2000 --hstore --number-processes 3 netherlands-latest.osm.pbf
@@ -17,6 +17,7 @@
 
 from __future__ import division
 import psycopg2
+import sys
 
 db = "gis"
 user = "gis"
@@ -40,9 +41,9 @@ try:
       ("Witteveen", 100),
       ("Shoeby", 223),
     ]
-  
-    con = psycopg2.connect(database='gis_nl', user='matthijs') 
-    cur = con.cursor() 
+
+    con = psycopg2.connect(database='gis_nl', user='matthijs')
+    cur = con.cursor()
 
     for shop in shops:
       cur.execute("SELECT COUNT(*) FROM planet_osm_polygon WHERE name LIKE %s AND NOT shop IS NULL;", (shop[0]+'%',))
@@ -54,9 +55,9 @@ try:
       print shop[0], total, shop[1], "%.2f" % percentage + '%'
 
 except psycopg2.DatabaseError, e:
-    print 'Error %s' % e    
+    print 'Error %s' % e
     sys.exit(1)
-    
+
 finally:
     if con:
         con.close()
